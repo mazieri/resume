7f62b56690c36e1346e5e6bc00549a013e81e0c8
mazieri <44025173+mazieri@users.noreply.github.com>
2023-12-17 17:32:44 -0300

    add convert all folder to pdf + docx

--- main.py
+++ main.py
@@ -1,9 +1,34 @@
+import os
 import pypandoc
 
+docs = "assets/"
 
-def convert_md_to_pdf(input_file, output_file):
-    output = pypandoc.convert_file(input_file, 'pdf', outputfile=output_file)
+
+def convert_md_to_pdf(input_to_pdf, output_to_pdf):
+    output = pypandoc.convert_file(input_to_pdf, "pdf", outputfile=output_to_pdf)
     assert output == ""
 
 
-convert_md_to_pdf('assets/resume_br.md', 'assets/resume_br.pdf')
+def convert_md_to_docx(input_to_docx, output_to_docx):
+    output = pypandoc.convert_file(input_to_docx, "docx", outputfile=output_to_docx)
+    assert output == ""
+
+
+for filename in os.listdir(docs):
+    if filename.endswith(".md"):
+        output_filename = filename.replace(".md", ".pdf")
+
+        input_file = os.path.join(docs, filename)
+        output_file = os.path.join(docs, output_filename)
+
+        convert_md_to_pdf(input_file, output_file)
+
+for filename in os.listdir(docs):
+    if filename.endswith(".md"):
+        output_filename = filename.replace(".md", ".docx")
+
+        input_file = os.path.join(docs, filename)
+        output_file = os.path.join(docs, output_filename)
+
+        convert_md_to_docx(input_file, output_file)
+
